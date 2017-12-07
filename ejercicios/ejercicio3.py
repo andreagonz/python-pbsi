1d12d05369556bf6b420760c04be00051762a949
andreagonz <andreagonz@ciencias.unam.mx>
2017-12-07 15:16:40 -0600

    ejercicio 3

---
+++ ejercicios/ejercicio3.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#UNAM-CERT
+from random import choice
+
+calificacion_alumno = {}
+calificaciones = (0,1,2,3,4,5,6,7,8,9,10)
+becarios = ['Alonso',
+            'Eduardo',
+            'Gerardo',
+            'Rafael',
+            'Antonio',
+            'Fernanda',
+            'Angel',
+            'Itzel',
+            'Karina',
+            'Esteban',
+            'Alan',
+            'Samuel',
+            'Jose',
+            'Guadalupe',
+            'Angel',
+            'Ulises']
+
+def asigna_calificaciones():
+    for b in becarios:
+        calificacion_alumno[b] = choice(calificaciones)
+
+def imprime_calificaciones():
+    for alumno in calificacion_alumno:
+        print '%s tiene %s\n' % (alumno,calificacion_alumno[alumno])
+
+asigna_calificaciones()
+imprime_calificaciones()
+
+def aprobados_reprobados():
+    reprobados = []
+    aprobados = []    
+    for alumno in calificacion_alumno:
+        if calificacion_alumno[alumno] >= 8:
+            aprobados.append(alumno)
+        else:
+            reprobados.append(alumno)
+    return (tuple(aprobados), tuple(reprobados))
+
+print aprobados_reprobados()
+
+def promedio():
+    p = 0.0
+    i = 0
+    for alumno in calificacion_alumno:
+        p += calificacion_alumno[alumno]
+        i += 1
+    return p / i
+
+print promedio()
+
+def conjunto_calificaciones():
+    print set(calificacion_alumno.values())
+
+print conjunto_calificaciones()
