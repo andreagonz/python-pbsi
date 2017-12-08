41bcfacac9271dfeef6c7907effe59275173df76
andreagonz <andreagonz@ciencias.unam.mx>
2017-12-08 15:30:20 -0600

    Cambio a ejercicio5 Agregando funcion que regresa cadena de las palabras en la concatenacion de las listas separadas por comas

--- ejercicios/ejercicio5.py
+++ ejercicios/ejercicio5.py
@@ -7,8 +7,9 @@ op_interna = ['quintero','fernando','yeudiel']
 incidentes = ['demian','anduin','diana','victor','vacante']
 auditorias = ['juan','fernando','oscar','daniel','gonzalo','cristian','jorge','virgilio']
 
-
 print (lambda a,b,c,d: map((lambda x:x.upper()), filter((lambda n: 'i' in n), (a + b + c + d)))) (sistemas,op_interna,incidentes,auditorias)
+
+print (lambda a,b,c,d: ','.join(map((lambda x:x.upper()), filter((lambda n: 'i' in n), (a + b + c + d))))) (sistemas,op_interna,incidentes,auditorias)
 
 #expresion funcional:
 # 1) funcion lambda que sume las cuatro listas
