c19e03a549308cd7c30a6aadb5e8ee117b63c1b5
andreagonz <andreagonz@ciencias.unam.mx>
2017-12-09 19:42:23 -0600

    cambios a tarea 3 map lamba map map mapmapma ya genera combinaciones de hasta n palabras en minusculas separadas por ciertos simbolos

--- tareas/tarea3/tarea3.py
+++ tareas/tarea3/tarea3.py
@@ -2,75 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from itertools import permutations, combinations
+from itertools import permutations, combinations, combinations_with_replacement, chain
 
 lst_simb_esp = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '|', '[', ']', '\\', ':', '"', ';', '\'', '<', '>', '?', ',', '.', '/', '', ' ']
 dicc_letras = {'a':['4','@'], 'i':['1', '!'], 's':['$','5'], 'e':['3'], 'o':['0']}
 lst_simbolos = ['#', '*', '_', '-', '.', '', ' ']
-    
-def permutaciones_str_lst(lst):
-    nueval = []
-    for l in lst:
-        nueval.append((l + " " + ' '.join([x for x in lst if x != l])).strip())
-    return nueval
 
-# Regresa lista de listas de tamaño n con combinaciones posibles de lista de símbolos
-def simb_comb_lst(n):
-    res = []
-    for x in range(1, n + 1):
-        res += [x for x in conjunto_potencia(lst_simbolos * n)
-                if len(x) == n - 1]
-    return res
-    
-def simb_comb_str(s):
+def combinaciones_str(s, comb_simb_lst):
     lst = []
-    for l in simb_comb_lst(len(s)):
-        ns = s
+    for l in comb_simb_lst[len(s.split()) - 1]:
+        x = s
         for simb in l:
-            ns.replace(' ', simb, 1)
-        lst.append(ns)
+            x = x.replace(' ', simb, 1)
+        lst.append(x)
     return lst
 
-def combinaciones_str_lst(lst):
-    res = []
-    for s in lst:
-        res += simb_comb_str(s)
-    return res
-
 def escribe_contrasenas(archivo, lst):
-    f = open(archivo[:[string.index(".")]] + "_contrasenas.txt", "w")
-    for l in lst:
-        f.write(l + "\n")
-    f.close()
-
-def comb_palabras(lst, n):
-    l = []
-    for x in range(1, n + 1):
-        l += combinations(lst, x)
-    return l
+    archivo_nuevo = archivo[:archivo.rfind(".")] + "_contrasenas.txt"
+    # with open(archivo_nuevo, "w") as f:
+        # map(lambda x: f.write(x + "\n"), lst)
+    print lst
+    print "Se generaron %d contraseñas" % len(lst)
 
 def dicc_constrasenas(archivo):
     f = open(archivo)
-    lst = f.readlines()
+    palabras = f.readlines()
     f.close()
-    lst = [x[:-1].lower() for x in lst]
-    # Conjunto potencia, lista de listas de cadenas
-    cp_lst = comb_palabras(lst, 3)
-    # Posibles permutaciones de listas del conjunto potencia, lista de cadenas con palabras
-    # separadas por espacios
-    lst_permutaciones = []
-    for l in cp_lst:
-        lst_permutaciones.append(permutaciones_str_lst(l))
-    return lst_permutaciones
-    # Combinaciones posibles de palabras cambiando letras por otros caracteres, lista de cadenas
-    lst_contrasenas = []
-    for l in lst_permutaciones:
-        lst_contrasenas += combinaciones_str_lst(l)
-    return lst_contrasenas
-
+    n = 3
+    palabras = [x[:-1].lower() for x in palabras]
+    # Lista de combinaciones de tamaño 0 a n con repetición de la lista de símbolos
+    comb_simb_lst = [list(combinations_with_replacement(lst_simbolos, x)) for x in range(0, n)]
+    # Lista de tuplas de combinaciones de palabras posibles de 1 a n tamaño
+    combinaciones_lst = []
+    map(lambda x: map(lambda s: combinaciones_lst.append(s), combinations(palabras, x)), range(1, n + 1))
+    # Lista de permutaciones posibles para cada tupla de la lista de combinaciones
+    permutaciones_lst = []
+    map(lambda l: map(lambda x: permutaciones_lst.append(' '.join(x)), permutations(l)), combinaciones_lst)
+    # Lista de contraseñas posibles por cada permutación
+    contrasenas_lst = []
+    map(lambda s: map(lambda x: contrasenas_lst.append(x), combinaciones_str(s, comb_simb_lst)), permutaciones_lst)
+    return contrasenas_lst
     
 if len(sys.argv) > 1:
-    print dicc_constrasenas(sys.argv[1])
+    archivo = sys.argv[1]
+    escribe_contrasenas(archivo, set(dicc_constrasenas(archivo)))
 else:
     print "Uso: python tarea3.py <archivo.txt>"
-
