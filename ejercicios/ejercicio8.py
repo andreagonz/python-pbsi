6a012a9e5b295f4362fa82da96a6b58ce4f207c6
andreagonz <andreagonz@ciencias.unam.mx>
2017-12-14 10:10:29 -0600

    ejercicio 8

--- ejercicios/ejercicio8.py
+++ ejercicios/ejercicio8.py
@@ -1,7 +1,7 @@
 from re import findall, search
 
-dirIPv4 = r"([0-9]{1,3}\.){3}[0-9]{1,3}"
-correo = r"[a-zA-Z0-9]+@([a-z]+\.)+[a-z]+"
+dirIPv4 = r"(?:[0-9]{1,3}\.){3}[0-9]{1,3}"
+correo = r"[a-zA-Z0-9]+(?:[\.\_-]?[a-zA-Z0-9]+)*@(?:[a-z]+\.)+[a-z]+"
 
-print findall(dirIPv4, "123.144.13.1")
-print findall(correo, "hola@hola.com")
+print findall(dirIPv4, "123.144.13.1 0.0.0.0")
+print findall(correo, "ola.hola-2_42.dads24-dd.asAD424@hola.com.mx")
